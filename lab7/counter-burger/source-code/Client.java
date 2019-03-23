4ac17f82e17d6476b0d1af786ef274abf1fa66b3
Anjali <anjali@RWC-WS30.local>
2019-03-23 13:11:34 -0700

    Initial commit for lab7

---
+++ lab7/counter-burger/source-code/Client.java
@@ -0,0 +1,16 @@
+  
+
+public class Client {
+
+    public static void runTest()
+    {
+        Component theOrder = BuildOrder.getOrder() ;
+        theOrder.printDescription();
+
+    }
+
+    public static void main(String args[]){
+        runTest();
+    }
+}
+ 
